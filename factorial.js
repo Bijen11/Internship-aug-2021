56b3897442514d6ded0360c8619244fb0f983edd
Bijen11 <42229292+Bijen11@users.noreply.github.com>
2021-08-04 11:14:57 +0545

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,9 @@
+let a = 5
+let sum=1;
+for(let i=1;i<=a;i++){
+     sum= sum *i;
+
+    
+    
+}
+console.log(sum);
---
+++ fibonacci.js
@@ -0,0 +1,11 @@
+
+let a = 5;
+let num1 =0, num2 =1 ,temp;
+console.log(num1);
+for(let i = 1; i <=a; i++) {
+   temp = num1+num2;
+   num1=num2;
+   num2=temp;
+   console.log(num1);
+
+}
